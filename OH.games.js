71d0753397d53520b9c8d1d3fdcd8d5f4cba8a7b
Bart Langelaan <langelaanbart@gmail.com>
2015-11-28 17:50:03 +0100

    Rewrite to use moment.js

--- OH.games.js
+++ OH.games.js
@@ -1,42 +1,37 @@
-var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
 OH = typeof OH !== "undefined" ? OH : {};
 OH.games = {
     all: [],
     home: [],
     refresh: function(){
         $.get("https://crossorigin.me/http://oliveohandbal.nl/programma/").then(function(data){
+            moment.locale('en');
+
             OH.games.all = $(data).find(".programmaoverzichtgegevens").map(function(){
                 var $td = $(this).find("td");
 
                 return {
-                    date: $td.eq(0).text(),
+                    time: moment($td.eq(0).text() + " " + $td.eq(2).text(), "D MMM HH:mm"),
                     home: $td.eq(1).text() == "thuis",
-                    time: $td.eq(2).text(),
                     team: $td.eq(3).text(),
                     opponent: $td.eq(4).text()
                 };
             });
 
             OH.games.home = $.grep(OH.games.all, function(wedstrijd, i){
-                var date = new Date();
-                return wedstrijd.home
-                    && wedstrijd.date.split(" ")[0] == date.getDate()
-                    && wedstrijd.date.split(" ")[1] == monthNames[date.getMonth()];
+                return wedstrijd.home && wedstrijd.time.isSame(moment(), 'day');
             });
+
+            moment.locale('nl');
         })
     },
-    getActive: function(date){
-        var actieveWedstrijd = 0;
+    getActive: function(){
+        var active = 0;
 
         for(i = 0; i < OH.games.home.length; i++){
-            var timeSplit = OH.games.home[i].time.split(":");
-            var hour = parseInt(timeSplit[0]);
-            var minute = parseInt(timeSplit[1]);
-
-            if(hour < date.getHours() || (hour == date.getHours() && minute <= date.getMinutes()))
-                actieveWedstrijd = i;
+            if(moment().isAfter(OH.games.home[i].time))
+                active = i;
         }
-        return actieveWedstrijd;
+
+        return active;
     }
 };
