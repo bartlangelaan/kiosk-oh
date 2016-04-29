18e8f5d33f57b3258d2f200d1b336c146c46865f
Bart Langelaan <langelaanbart@gmail.com>
2016-04-29 15:06:47 +0200

    Move all scripts to scripts.js

---
+++ scripts.js
@@ -0,0 +1,37 @@
+function writeScript(src){
+    document.write('<script src="' + src + '"></script>')
+}
+
+writeScript("http://code.jquery.com/jquery-2.1.4.min.js");
+writeScript("https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js");
+writeScript("https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-animate.min.js");
+writeScript("https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-aria.min.js");
+writeScript("https://ajax.googleapis.com/ajax/libs/angular_material/1.0.0-rc5/angular-material.min.js");
+writeScript("https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js");
+writeScript("https://l2.io/ip.js?var=ip");
+writeScript("kiosk.js");
+writeScript("kiosk.slides.js");
+writeScript("kiosk.oh.js");
+
+setInterval(function(){
+    
+    try{
+        var currPath = document.location.pathname.split('/');
+        currPath = currPath[currPath.length-1];
+
+        console.log(currPath);
+
+        if(moment().isBetween("2016-04-29 20:00", "2016-04-30 09:00") && currPath != "index.wandelevenement.html"){
+            document.location = "index.wandelevenement.html";
+        }
+        else if(currPath != "index.html"){
+            document.location = "index.html";
+        }
+        else{
+            var module = angular.module("Kiosk");
+        }
+    }
+    catch(error) {
+        document.location.reload();
+    }
+}, 30*1000);
